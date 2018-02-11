ed64f6d7354b33777286100c05f29ae53288ab9c
antoniomefa <antonio.mefa@outlook.com>
2018-02-11 08:56:11 -0600

    Modificación final
    Se agregó un intent e-mail para enviar la orden por correo. Se modificó el tema de la app. Se agregó traducción al español y alemán además del inglés.

--- app/src/main/java/com/antoniomendiola/justjava/MainActivity.java
+++ app/src/main/java/com/antoniomendiola/justjava/MainActivity.java
@@ -8,19 +8,22 @@ package com.antoniomendiola.justjava;
  *
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         EditText name = (EditText) findViewById(R.id.client_name);
         String clientName = name.getText().toString();
 
-        int price = calculatePrice();
+        int price = calculatePrice(hasChocolate, hasWhippedCream);
         createOrderSumary(price, hasWhippedCream, hasChocolate, clientName);
     }
 
@@ -50,17 +53,32 @@ public class MainActivity extends AppCompatActivity {
      * Calculates the price of the order.
      *
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addChocolate, boolean addWippedCream) {
+        int basePrice = 5;
+        if (addChocolate){
+            basePrice += 2;
+        }
+        if (addWippedCream){
+            basePrice += 1;
+        }
+        return quantity * basePrice;
     }
 
     public void increment(View view){
-        quantity += 1;
+        if(quantity == 100){
+            Toast.makeText(getApplicationContext(), getString(R.string.too_many_coffees), Toast.LENGTH_SHORT).show();
+        }else{
+            quantity += 1;
+        }
         displayQuantity(quantity);
     }
 
     public  void  decrement(View view){
-        quantity -= 1;
+        if(quantity == 1){
+            Toast.makeText(this, getString(R.string.too_few_coffees), Toast.LENGTH_SHORT).show();
+        }else{
+            quantity -= 1;
+        }
         displayQuantity(quantity);
     }
 
@@ -78,25 +96,24 @@ public class MainActivity extends AppCompatActivity {
      *@param price of order
      *@param addChocolate is whether or not the user wants chocolate topping
      *@param addWhippedCream is whether or not the user wants whipped cream topping
-     *@return text summary
      */
-    private String createOrderSumary (int price, boolean addWhippedCream, boolean addChocolate, String clientName){
+    private void createOrderSumary (int price, boolean addWhippedCream, boolean addChocolate, String clientName){
 
-        String priceMessage = "Name: " + clientName +
-                              "\nAdd whipped cream? " + addWhippedCream +
-                             "\nAdd chocolate? " + addChocolate +
-                              "\nQuantity: " + quantity +
-                              "\nTotal: $" + price +
-                              "\nThank You!";
-        return displayMessage(priceMessage);
+        String priceMessage = getString(R.string.order_summary_name, clientName) +
+                              "\n" + getString(R.string.order_summary_whipped_cream, addWhippedCream) +
+                             "\n" + getString(R.string.order_summary_chocolate, addChocolate) +
+                              "\n" + getString(R.string.order_summary_quantity, quantity) +
+                              "\n" + getString(R.string.order_summary_price, price) +
+                              "\n" + getString(R.string.thank_you);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, "emailaddress@emailaddress.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_email_subject, clientName));
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(Intent.createChooser(intent, "Send Email"));
+        }
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private String displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-        return message;
-    }
 }
